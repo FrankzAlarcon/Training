396f5c8b12b8db9a880654172d121301689c6235
Frankz <leninalarcon7@gmail.com>
2021-10-15 21:04:09 -0500

    finished js in figures page

--- js/figures.js
+++ js/figures.js
@@ -1,39 +1,265 @@
-import {errorMessage} from './general.js';
+import {errorMessage,showResult} from './general.js';
 
 
 document.addEventListener('DOMContentLoaded',()=>{
-  const buttonCalculatePerimeter = document.querySelector('.calculatePerimeterTriangle');
-  buttonCalculatePerimeter.addEventListener('click',(e)=>{
-    trianglePerimeter();
-  });
+  initApp();
 });
 
-/** Triangle */
+function initApp(){
+  /**Triangle buttons Events*/
 
+  //Calculate Perimeter
+  const buttonTrianglePerimeter = document.querySelector('.calculatePerimeterTriangle');
+  buttonTrianglePerimeter.addEventListener('click', ()=>{
+    const result = trianglePerimeter();
+    if(result){
+      showResult('../img/triangle.png',`El perímetro es: <span class="bold">${result}cm<span>`);
+    }
+  });
+
+  //Calculate Area
+  const buttonTriangleArea = document.querySelector('.calculateAreaTriangle');
+  buttonTriangleArea.addEventListener('click', ()=>{
+    const result = triangleArea();
+    if(result){
+      showResult('../img/triangle.png',`El área es: <span class="bold">${result}cm&sup2<span>`);
+    }
+  });
+
+  /**Square Buttons Events*/
+
+  //Calculate Perimeter
+  const buttonSquarePerimeter = document.querySelector('.calculateSquarePerimeter');
+  buttonSquarePerimeter.addEventListener('click',()=>{
+    const result = squareOperation('perimeter');
+    if(result){
+      showResult('../img/square.png', `El perímetro es: <span class="bold">${result}cm<span>`);
+    }
+  });
+
+  //Calculate Area
+  const buttonSquareArea = document.querySelector('.calculateSquareArea');
+  buttonSquareArea.addEventListener('click',()=>{
+    const result = squareOperation('area');
+    if(result){
+      showResult('../img/square.png', `El área es: <span class="bold">${result}cm&sup2<span>`);
+    }
+  });
+
+  /**Rectangle Buttons Events */
+
+  //Calculate Perimeter
+  const buttonRectanglePerimeter = document.querySelector('.calculate-rectangle-perimeter');
+  buttonRectanglePerimeter.addEventListener('click', ()=>{
+    const result = rectangleOperation('perimeter');
+    if(result){
+      showResult('../img/cube.png', `El perímetro es: <span class="bold">${result}cm<span>`);
+    }
+  });
+
+  const buttonRectangleArea = document.querySelector('.calculate-rectangle-area');
+  buttonRectangleArea.addEventListener('click',()=>{
+    const result = rectangleOperation('area');
+    if(result){
+      showResult('../img/cube.png', `El área es: <span class="bold">${result}cm&sup2<span>`);
+    }
+  });
+
+  /**Circle Buttons Events */
+  const buttonCirclePerimeter = document.querySelector('.calculate-perimeter-circle');
+  buttonCirclePerimeter.addEventListener('click', ()=>{
+    const result = circleOperation('perimeter').toFixed(2);
+    if(result){
+      showResult('../img/circulo-formula-perimetro-radio.png', `El perímetro es: <span class="bold">${result}cm<span>`);
+    }
+  });
+
+  const buttonCircleArea = document.querySelector('.calculate-are-circle');
+  buttonCircleArea.addEventListener('click',()=>{
+    const result = circleOperation('area').toFixed(2);
+    if(result){
+      showResult('../img/circulo-formula-perimetro-radio.png', `El área es: <span class="bold">${result}cm&sup2<span>`);
+    }
+  });
+
+  const buttonCircleDiameter = document.querySelector('.calculate-diameter-circle');
+  buttonCircleDiameter.addEventListener('click', ()=>{
+    const result = circleOperation('diameter').toFixed(2);
+    if(result){
+      showResult('../img/circulo-formula-perimetro-radio.png', `El diámetro es: <span class="bold">${result}cm<span>`);
+    }
+  });
+}
+
+/** Triangle functions*/
 function trianglePerimeter(){
   try{
+    /**Get container for alert messages */
+    const triangleContainer = document.querySelector('.alert-triangle');
+
+    /**Get inputs */
     let sideA = document.querySelector('#triangle-side-a');
     let sideB = document.querySelector('#triangle-side-b');
-    let base = document.querySelector('#triangle-base');
-    console.log(`${typeof sideA.value} ${sideB.value} ${base.value}`);
+    let base = document.querySelector('#triangle-base'); 
+
     if(!sideA.value || !sideB.value || !base.value){
       //Error: enter valid numbers
-      const triangleConainer = document.querySelector('.alert-triangle');
-      errorMessage('Ingrese números válidos',triangleConainer);
+      errorMessage('Ingrese los datos necesarios',triangleContainer);
       return;
     }
+
+    /**Transform input values to number */
     sideA = Number(sideA.value);
     sideB = Number(sideB.value);
     base = Number(base.value);
-    console.log(`${sideA} ${sideB} ${base}`);
 
     if(sideA <= 0 || sideB <= 0 || base<=0){
-      console.log('estoy en if')
+      /**If the numbers are not valid */
+      errorMessage('Los números deben ser mayores a 0', triangleContainer)
+      return ;
+    }
+    return sideA + sideB + base;
+  }catch(error){
+    console.log(error)
+    const triangleContainer = document.querySelector('.alert-triangle');
+    errorMessage('Ingrese números válidos',triangleContainer);
+  }
+}
 
+function triangleArea(){
+  try{
+    /**Get container for alert messages */
+    const triangleContainer = document.querySelector('.alert-triangle');
+
+    /**Get inputs */
+    let base = document.querySelector('#triangle-base'); 
+    let height = document.querySelector('#triangle-height');
+    if(!base.value || !height.value){
+      //Error: enter valid numbers
+      errorMessage('Ingrese los datos necesarios',triangleContainer);
+      return;
+    }
+
+    /**Transform input values to number */
+    base = Number(base.value);
+    height = Number(height.value);
+    if(base<=0 || height<=0){
+      /**If the numbers are not valid */
+      errorMessage('Los números deben ser mayores a 0', triangleContainer)
+      return ;
+    }
+    return base*height;
+  }catch(error){
+    console.log(error)
+    const triangleContainer = document.querySelector('.alert-triangle');
+    errorMessage('Ingrese números válidos',triangleContainer);
+  }
+}
+
+/** Square functions*/
+function squareOperation(operation){
+  try{
+    /**Get container for alert messages */
+    const squareContainer = document.querySelector('.alert-square');
+
+    /**Get inputs */
+    let side = document.querySelector('#square-side'); 
+
+    if(!side.value){
+      //Error: enter valid numbers
+      errorMessage('Ingrese los datos necesarios',squareContainer);
+      return;
+    }
+
+    /**Transform input values to number */
+    side = Number(side.value);
+
+    if(side<=0){
+      /**If the numbers are not valid */
+      errorMessage('Los números deben ser mayores a 0', squareContainer)
+      return ;
+    }
+    if(operation === 'perimeter'){
+      return side*4;
+    }else if(operation === 'area'){
+      return side*side;
     }
   }catch(error){
     console.log(error)
-    const triangleConainer = document.querySelector('.alert-triangle');
-    errorMessage('Ingrese números válidoddds',triangleConainer);
+    const squareContainer = document.querySelector('.alert-square');
+    errorMessage('Ingrese números válidos',squareContainer);
+  }
+}
+
+/**Rectangle functions */
+function rectangleOperation(operation){
+  try{
+    /**Get container for alert messages */
+    const rectangleContainer = document.querySelector('.alert-rectangle');
+
+    /**Get inputs */
+    let height = document.querySelector('#rectangle-height'); 
+    let width = document.querySelector('#rectangle-width');
+
+    if(!height.value || !width.value){
+      //Error: enter valid numbers
+      errorMessage('Ingrese los datos necesarios',rectangleContainer);
+      return;
+    }
+
+    /**Transform input values to number */
+    height = Number(height.value);
+    width = Number(width.value);
+    if(height<=0 || width <=0){
+      /**If the numbers are not valid */
+      errorMessage('Los números deben ser mayores a 0', rectangleContainer)
+      return ;
+    }
+    if(operation === 'perimeter'){
+      return height*2+width*2;
+    }else if(operation === 'area'){
+      return height*width;
+    }
+  }catch(error){
+    console.log(error)
+    const rectangleContainer = document.querySelector('.alert-rectangle');
+    errorMessage('Ingrese números válidos',rectangleContainer);
+  }
+}
+
+/**Circle functions */
+function circleOperation(operation){
+  try{
+    /**Get container for alert messages */
+    const circleContainer = document.querySelector('.alert-circle');
+
+    /**Get inputs */
+    let radio = document.querySelector('#cicle-r'); 
+
+    if(!radio.value){
+      //Error: enter valid numbers
+      errorMessage('Ingrese los datos necesarios',circleContainer);
+      return;
+    }
+
+    /**Transform input values to number */
+    radio = Number(radio.value);
+
+    if(radio<=0){
+      /**If the numbers are not valid */
+      errorMessage('Los números deben ser mayores a 0', circleContainer)
+      return ;
+    }
+    if(operation === 'perimeter'){
+      return 2*radio*Math.PI;
+    }else if(operation === 'area'){
+      return Math.PI*radio*radio;
+    } else if (operation === 'diameter'){
+      return radio*2;
+    }
+  }catch(error){
+    console.log(error)
+    const circleContainer = document.querySelector('.alert-square');
+    errorMessage('Ingrese números válidos',circleContainer);
   }
 }
--- js/general.js
+++ js/general.js
@@ -20,7 +20,69 @@ function errorMessage(message,container){
   },3000);
 
 }
+function showResult(dirImage, result){
+  resetInputs();
+  /**Add class to fixed body */
+  const body = document.querySelector('body');
+  body.classList.add('body-hidden');
 
+  /**create container for the result */
+  const container = document.createElement('DIV');
+  container.classList.add('container__result');
+
+  /**create children tags */
+  const close = document.createElement('P');
+  close.classList.add('close-button');
+  close.textContent = 'X';
+  const img = document.createElement('IMG');
+  img.classList.add('result__img');
+  img.src=dirImage;
+  const text = document.createElement('P');
+  text.classList.add('result__text');
+  text.innerHTML = result;
+
+  /**Add children to the DIV parent*/
+  container.appendChild(close);
+  container.appendChild(img);
+  container.appendChild(text);
+
+  /**Add container to the DOM */
+  const show = document.querySelector('.result-operations');
+  show.classList.add('show-result')
+  show.appendChild(container);
+
+  /**Close click in X */
+  close.onclick = ()=>{
+    body.classList.remove('body-hidden');
+    show.classList.remove('show-result');
+    container.remove();
+  };
+
+  /**Click in blue container */
+  let allowClose = true;
+  container.onclick = ()=>{
+    allowClose = false;
+  }
+
+  /**Close click out of blue box */
+  show.onclick = ()=>{
+    if(allowClose){
+      //if click out of blue container
+      body.classList.remove('body-hidden');
+      show.classList.remove('show-result');
+      container.remove();
+    }
+    allowClose = true;
+  }
+  
+}
+function resetInputs(){
+  const allInputs = document.querySelectorAll('input[type=number]');
+  allInputs.forEach((input)=>{
+    input.value='';
+  });
+}
 export{
-  errorMessage
+  errorMessage,
+  showResult
 }
